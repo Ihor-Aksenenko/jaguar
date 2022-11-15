3c3d2f42f5a37ea3bc2849eb8f42c88495091083
Ihor-Aksenenko <112870677+Ihor-Aksenenko@users.noreply.github.com>
2022-11-15 21:36:42 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,5 +2,16 @@ $(document).ready(function () {
    $('.header__burger').click(function (event) {
       $('.header__burger, .header__menu').toggleClass('active');
       $('body').toggleClass('body-menu');
+   }); 
+   $('.slider').slick({
+      arrows: true,
+      dots: true,
+      slidesToShow: 1,
+      autoplay: true,
+      speed: 1500,
+      autoplaySpeed: 3000,
+      pauseOnFocus: false,
+      pauseOnHover: false
    });
 });
+
